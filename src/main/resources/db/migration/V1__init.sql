e27fa484b8b8810bf8f523619e87b3877bc63225
ykmshhh <yakukhno.maria@lll.kpi.ua>
2025-12-08 11:08:06 +0200

    change card and transaction types on enam in db

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,6 +2,10 @@ CREATE TYPE account_type AS ENUM('credit', 'saving', 'deposit');
 
 CREATE TYPE loan_status AS ENUM('active', 'closed', 'overdue');
 
+CREATE TYPE card_type AS ENUM('debit', 'credit');
+
+CREATE TYPE transaction_type AS ENUM('deposit', 'transfer', 'withdraw');
+
 CREATE TABLE customer (
                           customer_id SERIAL PRIMARY KEY,
                           name VARCHAR(32) NOT NULL,
@@ -19,22 +23,17 @@ CREATE TABLE account (
                          customer_id INT NOT NULL REFERENCES customer(customer_id)
 );
 
-CREATE TABLE card_type (
-                           type_id SERIAL PRIMARY KEY,
-                           type_name VARCHAR(32) UNIQUE NOT NULL
-);
-
 CREATE TABLE card (
                       card_id SERIAL PRIMARY KEY,
                       card_number VARCHAR(16) NOT NULL UNIQUE,
                       expiration_date DATE NOT NULL,
-                      type_id INT NOT NULL REFERENCES card_type(type_id),
+                      type card_type NOT NULL,
                       account_id INT NOT NULL REFERENCES account(account_id)
 );
 
 CREATE TABLE transactions (
                               transaction_id SERIAL PRIMARY KEY,
-                              type VARCHAR(32) NOT NULL,
+                              type transaction_type NOT NULL,
                               currency VARCHAR(3) NOT NULL,
                               amount DECIMAL(12, 2) NOT NULL CHECK (amount > 0),
                               date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
