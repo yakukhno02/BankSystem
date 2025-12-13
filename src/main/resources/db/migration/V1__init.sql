0ec07b4bcb87ae48b06ff92ceaffc2b12e0aef82
ykmshhh <yakukhno.maria@lll.kpi.ua>
2025-12-13 10:50:59 +0200

    replace PostgreSQL enums with VARCHAR + CHECK constraints

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,11 +1,3 @@
-CREATE TYPE account_type AS ENUM('credit', 'saving', 'deposit');
-
-CREATE TYPE loan_status AS ENUM('active', 'closed', 'overdue');
-
-CREATE TYPE card_type AS ENUM('debit', 'credit');
-
-CREATE TYPE transaction_type AS ENUM('deposit', 'transfer', 'withdraw');
-
 CREATE TABLE customer (
                           customer_id SERIAL PRIMARY KEY,
                           name VARCHAR(32) NOT NULL,
@@ -19,7 +11,7 @@ CREATE TABLE account (
                          iban VARCHAR(34) NOT NULL UNIQUE,
                          balance DECIMAL(12, 2) DEFAULT 0,
                          currency VARCHAR(3) NOT NULL,
-                         type account_type NOT NULL,
+                         type VARCHAR(20) NOT NULL CHECK (type IN ('credit', 'saving', 'deposit')),
                          customer_id INT NOT NULL REFERENCES customer(customer_id)
 );
 
@@ -27,13 +19,13 @@ CREATE TABLE card (
                       card_id SERIAL PRIMARY KEY,
                       card_number VARCHAR(16) NOT NULL UNIQUE,
                       expiration_date DATE NOT NULL,
-                      type card_type NOT NULL,
+                      type VARCHAR(20) NOT NULL CHECK (type IN ('debit', 'credit')),
                       account_id INT NOT NULL REFERENCES account(account_id)
 );
 
 CREATE TABLE transactions (
                               transaction_id SERIAL PRIMARY KEY,
-                              type transaction_type NOT NULL,
+                              type VARCHAR(20) NOT NULL CHECK (type IN ('deposit', 'transfer', 'withdraw')),
                               currency VARCHAR(3) NOT NULL,
                               amount DECIMAL(12, 2) NOT NULL CHECK (amount > 0),
                               date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -48,6 +40,6 @@ CREATE TABLE loan (
                       interest_rate DECIMAL(5, 2) CHECK (interest_rate >= 0 AND interest_rate <= 100),
                       start_date DATE NOT NULL,
                       end_date DATE NOT NULL,
-                      status loan_status NOT NULL,
+                      status VARCHAR(20) NOT NULL CHECK(status IN ('active', 'closed', 'overdue')),
                       account_id INT NOT NULL REFERENCES account(account_id)
 );
