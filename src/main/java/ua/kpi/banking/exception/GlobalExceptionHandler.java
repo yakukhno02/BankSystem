ecee0ffe74ffbf03f79cdf1e0e2897034cc3007b
ykmshhh <yakukhno.maria@lll.kpi.ua>
2026-06-22 15:27:41 +0300

    feat: add validation error handler in GlobalExceptionHandler

--- src/main/java/ua/kpi/banking/exception/GlobalExceptionHandler.java
+++ src/main/java/ua/kpi/banking/exception/GlobalExceptionHandler.java
@@ -1,36 +1,46 @@
-//package ua.kpi.banking.exception;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-//import java.util.Map;
-//
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(NotFoundException.class)
-//    public ResponseEntity<?> handleNotFound(NotFoundException ex) {
-//        return ResponseEntity
-//                .status(HttpStatus.NOT_FOUND)
-//                .body(errorBody(
-//                        HttpStatus.NOT_FOUND.value(),
-//                        ex.getMessage()
-//                ));
-//    }
-//    @ExceptionHandler(BadRequestException.class)
-//    public ResponseEntity<?> handleBadRequest(BadRequestException ex) {
-//        return ResponseEntity
-//                .status(HttpStatus.BAD_REQUEST)
-//                .body(errorBody(
-//                        HttpStatus.BAD_REQUEST.value(),
-//                        ex.getMessage()
-//                ));
-//    }
-//
-//    private Map<String, Object> errorBody(int status, String message) {
-//        return Map.of(
-//                "status", status,
-//                "message", message
-//        );
-//    }
-//}
+package ua.kpi.banking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFound(NotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorBody(
+                        HttpStatus.NOT_FOUND.value(),
+                        ex.getMessage()
+                ));
+    }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorBody(
+                        HttpStatus.BAD_REQUEST.value(),
+                        ex.getMessage()
+                ));
+    }
+
+    private Map<String, Object> errorBody(int status, String message) {
+        return Map.of(
+                "status", status,
+                "message", message
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body(
+                Map.of("message", "Validation failed")
+        );
+    }
+}
