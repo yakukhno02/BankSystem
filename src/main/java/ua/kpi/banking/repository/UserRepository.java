9111c83dbbc3bb040e3ef069fe90db03f987e4c2
ykmshhh <yakukhno.maria@lll.kpi.ua>
2026-06-30 15:12:12 +0300

    feat: add User repository

---
+++ src/main/java/ua/kpi/banking/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ua.kpi.banking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.kpi.banking.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
